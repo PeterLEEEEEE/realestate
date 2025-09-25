ea19265b9bc38221aad989ab7c94b3893d8ced5a
PeterLEEEEEE <dissgogo@naver.com>
2025-09-26 01:32:03 +0900

    add mcp

--- src/server.py
+++ src/server.py
@@ -5,13 +5,15 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi.middleware import Middleware
 from contextlib import asynccontextmanager
+from fastmcp import FastMCP
 
 from src.router import router
 from src.container import AppContainer
+from src.core.mcp import register_basic_tools
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def app_lifespan(app: FastAPI):
     redis = Redis.from_url(
         "redis://:abc123@redis:6379", decode_responses=True
     )
@@ -20,17 +22,29 @@ async def lifespan(app: FastAPI):
     
 def load_middleware() -> list[Middleware]:
     ...
+    
+# Create MCP server
+mcp = FastMCP("Tools")
+register_basic_tools(mcp)
+mcp_app = mcp.http_app(path='/v1')
+
+
+@asynccontextmanager
+async def combined_lifespan(app: FastAPI):
+    async with app_lifespan(app):
+        async with mcp_app.lifespan(app):
+            yield
 
 def create_app() -> FastAPI:
     container = AppContainer()
     app_ = FastAPI(
-        title="LangGraph Practical API",
+        title="LangGraph Realestate Agent API",
         description="FastAPI with dependency injection",
-        lifespan=lifespan,
+        lifespan=combined_lifespan,
         # middleware=load_middleware,
         debug=True
     )
-    
+    app_.mount("/mcp", mcp_app)
     # 이 부분이 의존성 주입을 활성화
     container.wire(
         packages=["src.domain"], # 폴더 단위
